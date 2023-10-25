5463c7c5b41757c8e6551734ac9f7248580fadae
Huy Trinh <huy.trinh20102001@gmail.com>
2023-10-25 12:57:14 +0700

    adjust the button 2 3

--- Core/Inc/button.h
+++ Core/Inc/button.h
@@ -13,10 +13,13 @@
 #define NORMAL_STATE SET
 #define PRESSED_STATE RESET
 
+extern int button1_flag;
+extern int button2_flag;
+extern int button3_flag;
+
 int isButton1Pressed();
 int isButton2Pressed();
 int isButton3Pressed();
-
 
 void getKeyInput1(void);
 void getKeyInput2(void);
